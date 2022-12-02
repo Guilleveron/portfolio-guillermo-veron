14713b003e1316e82ebb2f5605860e2da749bbcf
Guillermo Veron <guillermo.veron@biggie.com.py>
2022-12-02 15:02:52 -0300

    Projects added

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-blog',
+    title: 'Portfolio Guillermo Verón',
     htmlAttrs: {
       lang: 'en',
     },
